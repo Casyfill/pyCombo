ee21d53447253d49d81778ccdf47673ee0fc6b84
Philipp Kats <casyfill@gmail.com>
2016-04-25 13:56:27 -0400

    temporal

--- pycombo/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from pyCombo.version import version as __version__
-except ImportError:
-    __version__ = '0.1.0.dev-unknown'
-
-from pyCombo import getComboPartition as combo
-from pyCombo import modularity
