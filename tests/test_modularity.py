0e4baf8869881f8bf4b32b3ecdbfb198eb3ec6e5
Philipp <casyfill@gmail.com>
2020-11-28 16:35:47 -0500

    different modularity

--- tests/test_modularity.py
+++ tests/test_modularity.py
@@ -2,15 +2,21 @@ import pytest
 
 def test_modularity_test_graph(test_graph):
     from pyCombo import combo #, modularity
+    from community import modularity as modularity_metric
 	
-    _, modularity_ = combo(test_graph, weight_prop='weight')
+    partition, modularity_ = combo(test_graph, weight_prop='weight')
     assert modularity_ == pytest.approx(0.16, 0.0001)
+
+    lv_modularity = modularity_metric(partition, test_graph) 
+    assert modularity_ == lv_modularity
     # assert modularity_ == pytest.approx(modularity(test_graph, partition, key='weight') )
 
 
 
 def test_modularity_karate(karate):
     from pyCombo import combo
+    
 
     _, modularity_ = combo(karate)
     assert modularity_ == 0.41979
+    
