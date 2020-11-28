b432f3d30dec55524bdc87cbca7524c516f6a667
Philipp <casyfill@gmail.com>
2020-11-28 16:21:07 -0500

    flake8 blackified

--- tests/test_modularity.py
+++ tests/test_modularity.py
@@ -4,7 +4,7 @@ import pytest
 def test_modularity_test_graph(test_graph):
     from pyCombo import combo  # , modularity
 
-    _, modularity_ = combo(test_graph, weight_prop='weight')
+    _, modularity_ = combo(test_graph, weight_prop="weight")
     assert modularity_ == pytest.approx(0.16, 0.0001)
     # assert modularity_ == pytest.approx(modularity(test_graph, partition, key='weight') )
 
@@ -12,5 +12,5 @@ def test_modularity_test_graph(test_graph):
 def test_modularity_karate(karate):
     from pyCombo import combo
 
-    _ , modularity_ = combo(karate)
+    _, modularity_ = combo(karate)
     assert modularity_ == 0.437212
