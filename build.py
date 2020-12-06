88b8d14b0945b7ce28b28e02060a0d7379d1368d
Philipp <casyfill@gmail.com>
2020-12-06 18:41:16 -0500

    fix

--- build.py
+++ build.py
@@ -4,10 +4,11 @@ from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatfo
 
 
 ext_modules = [
-    Extension("pyCombo.comboCPP",
-              include_dirs=["./src"],
-              sources=["src/Main.cpp", "src/Graph.cpp"],
-             ),
+    Extension(
+        "pyCombo.comboCPP",
+        include_dirs=["./src"],
+        sources=["src/Main.cpp", "src/Graph.cpp"],
+    ),
 ]
 
 
@@ -16,18 +17,22 @@ class BuildFailed(Exception):
 
 
 class ExtBuilder(build_ext):
-
     def run(self):
         try:
             build_ext.run(self)
         except (DistutilsPlatformError, FileNotFoundError):
-            raise BuildFailed('File not found. Could not compile C extension.')
+            raise BuildFailed("File not found. Could not compile C extension.")
 
     def build_extension(self, ext):
         try:
             build_ext.build_extension(self, ext)
-        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError) as e:
-            raise BuildFailed(f'Could not compile C extension: {e}')
+        except (
+            CCompilerError,
+            DistutilsExecError,
+            DistutilsPlatformError,
+            ValueError,
+        ) as e:
+            raise BuildFailed(f"Could not compile C extension: {e}")
 
 
 def build(setup_kwargs):
