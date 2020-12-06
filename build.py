9fc637c72efa256a20c84509b79bc939cc9a6b09
Philipp <casyfill@gmail.com>
2020-12-06 18:34:39 -0500

    crossplatform built try-1

---
+++ build.py
@@ -0,0 +1,39 @@
+from distutils.command.build_ext import build_ext
+from distutils.core import Extension
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
+
+ext_modules = [
+    Extension("pyCombo.comboCPP",
+              include_dirs=["./src"],
+              sources=["src/Main.cpp", "src/Graph.cpp"],
+             ),
+]
+
+
+class BuildFailed(Exception):
+    pass
+
+
+class ExtBuilder(build_ext):
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError):
+            raise BuildFailed('File not found. Could not compile C extension.')
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError) as e:
+            raise BuildFailed(f'Could not compile C extension: {e}')
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    setup_kwargs.update(
+        {"ext_modules": ext_modules, "cmdclass": {"build_ext": ExtBuilder}}
+    )
