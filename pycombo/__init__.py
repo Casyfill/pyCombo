962d89796546031282a67981ee32852473c2fdb5
Alexander-Belyi <78016743+Alexander-Belyi@users.noreply.github.com>
2021-02-10 12:55:59 +0300

    Updated __version__ in __init__. See this for more details: https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094

--- pycombo/__init__.py
+++ pycombo/__init__.py
@@ -1,4 +1,9 @@
-__version__ = "0.0.1"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
 
 from .pyCombo import get_combo_partition
 
