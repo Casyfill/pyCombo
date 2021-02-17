e5b26e911980203745f8999b1b243b24222b7c62
Alexander-Belyi <78016743+Alexander-Belyi@users.noreply.github.com>
2021-02-17 14:51:08 +0300

    Added a few tests. For weighted directed graphs, and for new modularity mode.

--- tests/test_pyCombo.py
+++ tests/test_pyCombo.py
@@ -41,3 +41,67 @@ def test_deconstruct_graph(karate):
 
     nodes, edges = deconstruct_graph(karate)
     assert len(nodes) == len(karate)
+    assert len(edges) == karate.size()
+
+
+def test_weighted_graph():
+    import networkx as nx
+    lesmis = nx.les_miserables_graph()
+    from pycombo import execute
+
+    _, modularity = execute(lesmis, random_seed=42)
+    assert modularity == pytest.approx(0.566688, 0.000001), modularity
+
+
+def test_weighted_digraph(block_model):
+    from pycombo import execute
+
+    partition, modularity = execute(block_model, random_seed=42)
+    assert modularity == pytest.approx(0.421296, 0.000001), modularity
+    assert _partitionGroup(partition) == [list(range(5)), list(range(5, 5 + 8)), list(range(5 + 8, 5 + 18))]
+
+
+def _get_modularity_matrix(graph, symmetrize=False):
+    '''
+    build modularity matrix and return as numpy array
+    '''
+    import numpy as np
+
+    A = nx.to_numpy_array(graph)
+    if not graph.is_directed():
+        A += np.diag(np.diag(A))
+    wout = A.sum(axis=1)
+    win = A.sum(axis=0)
+    T = wout.sum()
+    Q = A / T - np.matmul(wout.reshape(-1, 1), win.reshape(1, -1)) / (T ** 2)
+    if symmetrize:
+        Q = (Q + Q.transpose()) / 2
+    return Q
+
+
+def test_mod_matrix(karate):
+    from pycombo import execute
+
+    partition_g, modularity_g = execute(karate, random_seed=42)
+    mod_matrix = _get_modularity_matrix(karate)
+    partition_m, modularity_m = execute(mod_matrix.tolist(), treat_as_modularity=True, random_seed=42)
+    assert modularity_m == pytest.approx(modularity_g, 0.000001), (modularity_m, modularity_g)
+    assert _partitionGroup(partition_g) == _partitionGroup(partition_m)
+
+
+def test_mod_graph(karate):
+    from pycombo import execute
+
+    partition_g, modularity_g = execute(karate, random_seed=42)
+    mod_graph = nx.from_numpy_array(_get_modularity_matrix(karate), create_using=nx.DiGraph)
+    partition_m, modularity_m = execute(mod_graph, treat_as_modularity=True, random_seed=42)
+    assert modularity_m == pytest.approx(modularity_g, 0.000001), (modularity_m, modularity_g)
+    assert _partitionGroup(partition_g) == _partitionGroup(partition_m)
+
+
+def test_clique_partitioning(test_cp_graph):
+    import pycombo
+
+    partition, modularity = pycombo.execute(test_cp_graph, treat_as_modularity=True)
+    assert modularity == 1.1, modularity
+    assert _partitionGroup(partition) == _partitionGroup({0: 0, 1: 0, 2: 1, 3: 2})
