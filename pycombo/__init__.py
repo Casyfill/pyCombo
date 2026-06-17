25dfbd87bdfe878dafb7d7f82f5997f4d223667c
Philipp Kats <casyfill@gmail.com>
2026-06-17 13:18:11 -0400

    minor improvements

--- pycombo/__init__.py
+++ pycombo/__init__.py
@@ -1,8 +1,4 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
 
--- pycombo/misc.py
+++ pycombo/misc.py
@@ -9,63 +9,10 @@ edges_ = List[Tuple[int, int, float]]
 
 
 def is_graph(graph) -> bool:
-    graph_names = {"Graph", "DiGraph", "MultiGraph", "MultiDiGraph"}
-    return type(graph).__name__ in graph_names
-
-
-def is_weighted(G, edge: Optional[tuple] = None, weight: str = "weight") -> bool:
-    """Returns True if `G` has weighted edges.
-
-    NOTE: ventorized from networkx:
-    https://networkx.org/documentation/stable/_modules/networkx/classes/function.html#is_weighted
-
-    Parameters
-    ----------
-    G : graph
-        A NetworkX graph.
-
-    edge : tuple, optional
-        A 2-tuple specifying the only edge in `G` that will be tested. If
-        None, then every edge in `G` is tested.
-
-    weight: string, optional
-        The attribute name used to query for edge weights.
-
-    Returns
-    -------
-    bool
-        A boolean signifying if `G`, or the specified edge, is weighted.
-
-    Raises
-    ------
-    NetworkXError
-        If the specified edge does not exist.
-
-    Examples
-    --------
-    >>> G = nx.path_graph(4)
-    >>> nx.is_weighted(G)
-    False
-    >>> nx.is_weighted(G, (2, 3))
-    False
-
-    >>> G = nx.DiGraph()
-    >>> G.add_edge(1, 2, weight=1)
-    >>> nx.is_weighted(G)
-    True
-
+    """Tests if the input is a NetworkX graph.
+    Avoids having to import NetworkX just to check the type.
     """
-    if edge is not None:
-        data = G.get_edge_data(*edge)
-        if data is None:
-            raise ValueError(f"Edge {edge!r} does not exist.")
-        return weight in data
-
-    if not any(G.adj.values()):  # nx.is_empty()
-        # Special handling required since: all([]) == True
-        return False
-
-    return all(weight in data for u, v, data in G.edges(data=True))
+    return hasattr(graph, "nodes") and hasattr(graph, "edges") and hasattr(graph, "adj")
 
 
 def _has_weight_attribute(graph, weight: str) -> bool:
@@ -101,11 +48,10 @@ def deconstruct_graph(graph, weight: Optional[str] = None) -> Tuple[nodes_, edge
         nodes[i] = n
 
     edges = []
-    weight_key = weight if weight is not None else "weight"
     for edge in graph.edges(data=True):
         if weight is None:
             edge_weight = default_weight
         else:
-            edge_weight = edge[2].get(weight_key, default_weight)
+            edge_weight = edge[2].get(weight, default_weight)
         edges.append((nodenum[edge[0]], nodenum[edge[1]], edge_weight))
     return nodes, edges
